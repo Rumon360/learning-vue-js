5aa45498b262af426b7ac0b44eaaca05d6bef55c
Rumon360 <hmk.rumon@gmail.com>
2025-03-12 22:27:11 +0600

    add hero section

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import Navbar from "@/components/navbar.vue";
+import Hero from "@/components/hero.vue";
 </script>
 
 <template>
   <Navbar />
+  <Hero subtitle="Find the Vue job that fits your skills and needs" />
 </template>
