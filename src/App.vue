39ae5374393df0ba9bff1d22e6e464bebe7f86a8
Rumon360 <hmk.rumon@gmail.com>
2025-03-13 01:09:51 +0600

    add home-cards and job listings

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
 import Navbar from "@/components/navbar.vue";
 import Hero from "@/components/hero.vue";
+import HomeCards from "@/components/home-cards.vue";
+import JobListings from "@/components/job-listings.vue";
 </script>
 
 <template>
   <Navbar />
   <Hero subtitle="Find the Vue job that fits your skills and needs" />
+  <HomeCards />
+  <JobListings />
 </template>
